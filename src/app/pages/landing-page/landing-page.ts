49c670c118c92c8d2400339976c39f43937e8c1a
Timo <timoboen@gmail.com>
2026-06-14 14:02:48 +0200

    style: hero styling (refined behavior for screen above 1440 px)

--- src/app/pages/landing-page/landing-page.ts
+++ src/app/pages/landing-page/landing-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Hero } from '../../sections/hero/hero';
+import { About } from '../../sections/about/about';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [Hero],
+  imports: [Hero, About],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
 })
