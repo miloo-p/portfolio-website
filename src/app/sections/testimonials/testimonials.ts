57ca0e6037b0eca3cf4ec72c829af89885252cde
Timo <timoboen@gmail.com>
2026-06-19 11:01:21 +0200

    feat: added & styled testemonial section - added contact section

--- src/app/sections/testimonials/testimonials.ts
+++ src/app/sections/testimonials/testimonials.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
 
+interface Testimonial {
+  name: string;
+  text: string;
+}
+
 @Component({
   selector: 'app-testimonials',
   imports: [],
   templateUrl: './testimonials.html',
   styleUrl: './testimonials.scss',
 })
-export class Testimonials {}
+export class Testimonials {
+  public myTestemonials: Testimonial[] = [
+    {
+      name: 'Jemand Wichtiges, Developer',
+      text: 'Dies ist ein absolut offensichtlicher Platzhalter-Text. Stell dir einfach vor, hier würde jetzt eine echte Person sehr enthusiastisch über meine Problemlösungs-Skills, meine Zuverlässigkeit und meinen Background als Mediengestalter berichten. Klingt gut, oder? Das echte Zitat folgt in Kürze.',
+    },
+    {
+      name: 'Jemand Wichtiges, Developer',
+      text: 'Dies ist ein absolut offensichtlicher Platzhalter-Text. Stell dir einfach vor, hier würde jetzt eine echte Person sehr enthusiastisch über meine Problemlösungs-Skills, meine Zuverlässigkeit und meinen Background als Mediengestalter berichten. Klingt gut, oder? Das echte Zitat folgt in Kürze.',
+    },
+    {
+      name: 'Jemand Wichtiges, Developer',
+      text: 'Dies ist ein absolut offensichtlicher Platzhalter-Text. Stell dir einfach vor, hier würde jetzt eine echte Person sehr enthusiastisch über meine Problemlösungs-Skills, meine Zuverlässigkeit und meinen Background als Mediengestalter berichten. Klingt gut, oder? Das echte Zitat folgt in Kürze.',
+    },
+  ];
+}
