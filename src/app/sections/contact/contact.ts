823f18c30f8f964fccddb7944ecab53ac43f14ca
Timo <timoboen@gmail.com>
2026-06-22 16:22:30 +0200

    feat: implement HTML & Style for contact form

--- src/app/sections/contact/contact.ts
+++ src/app/sections/contact/contact.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { BtnCtaPrimary } from '../../shared/components/btn-cta-primary/btn-cta-primary';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [BtnCtaPrimary],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
