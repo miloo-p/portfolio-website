fd61f0b6fa01b8ef45fb06b811243b69d239f9b6
Timo <timoboen@gmail.com>
2026-06-23 22:48:37 +0200

    feat: prototype for the burger menu

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { PageNavigation } from '../../shared/components/page-navigation/page-navigation';
 import { BtnCtaPrimary } from '../../shared/components/btn-cta-primary/btn-cta-primary';
 
 @Component({
   selector: 'app-header',
-  imports: [PageNavigation, BtnCtaPrimary],
+  imports: [CommonModule, PageNavigation, BtnCtaPrimary],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  isMenuOpen = signal(false);
+
+  toggleMenu() {
+    this.isMenuOpen.update((val) => !val);
+  }
+
+  // Schließt das Overlay, wenn der Screen wieder Desktop-Größe erreicht
+  @HostListener('window:resize')
+  onResize() {
+    if (window.innerWidth > 1200 && this.isMenuOpen()) {
+      this.isMenuOpen.set(false);
+    }
+  }
+}
