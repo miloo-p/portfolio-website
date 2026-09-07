cfc97749a819eb7a61dff6d567d27f852a144469
Timo <timoboen@gmail.com>
2026-09-07 12:46:33 +0200

    feat: optimized text

--- src/app/shared/components/page-navigation/page-navigation.ts
+++ src/app/shared/components/page-navigation/page-navigation.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, signal } from '@angular/core';
+import { Component, HostListener, signal, Output, EventEmitter } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -9,15 +9,20 @@ import { RouterLink } from '@angular/router';
 })
 export class PageNavigation {
   isResizing = signal(false);
-
   private resizeTimer: any;
+
+  // NEU: Output-Event definieren
+  @Output() linkClicked = new EventEmitter<void>();
+
+  // NEU: Methode zum Abfeuern des Events
+  closeMenu() {
+    this.linkClicked.emit();
+  }
 
   @HostListener('window:resize')
   onResize() {
     this.isResizing.set(true);
-
     clearTimeout(this.resizeTimer);
-
     this.resizeTimer = setTimeout(() => {
       this.isResizing.set(false);
     }, 200);
